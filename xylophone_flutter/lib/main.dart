545d4ca8e0c4fc9b0c09162998e17ec3e4065b47
asikkolorob <asikkolorob@gmail.com>
2021-12-07 21:46:43 +0600

    quize app

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -1,22 +1,51 @@
 import 'package:flutter/material.dart';
+// import 'package:audioplayers/audioplayers.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  void funtionsOfCon(int numberOfCon) {
+    // ignore: avoid_print
+    print("Container $numberOfCon Cliked");
+  }
+
+  dynamic buildKey({required Color color, int? printNum}) {
+    return Expanded(
+      child: InkWell(
+        onTap: () {
+          funtionsOfCon(printNum!);
+        },
+        child: Container(
+          color: color,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.cyan,
+        backgroundColor: Colors.black,
         appBar: AppBar(
           elevation: 0.0,
           backgroundColor: Colors.cyan.shade400,
           title: const Text("data"),
+        ),
+        body: Column(
+          children: [
+            buildKey(color: Colors.teal, printNum: 1),
+            buildKey(color: Colors.red, printNum: 2),
+            buildKey(color: Colors.cyan, printNum: 3),
+          ],
         ),
       ),
     );
