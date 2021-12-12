959b28cbb8eb778c6554af64ea3651ea1335c167
asikkolorob <asikkolorob@gmail.com>
2021-12-12 18:22:34 +0600

    new  app add

--- quizzler_flutter/lib/main.dart
+++ quizzler_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'quiz_brain.dart';
 
 Quizbrain quizeBrain = Quizbrain();
@@ -35,6 +36,37 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreIcon = [];
 
+  void checkAnswer(bool userPickedAnswer) {
+    // ignore: unused_local_variable
+    bool? currectAnswer = quizeBrain.currectAnswer();
+    setState(() {
+      if (quizeBrain.isFinished() == true) {
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+        ).show();
+
+        quizeBrain.reset();
+
+        scoreIcon = [];
+      } else {
+        if (userPickedAnswer == currectAnswer) {
+          scoreIcon.add(const Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scoreIcon.add(const Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+        quizeBrain.nextQuestion();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,9 +83,10 @@ class _QuizPageState extends State<QuizPage> {
                   quizeBrain.getQuestions().toString(),
                   textAlign: TextAlign.center,
                   style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.bold),
+                    color: Colors.white,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
@@ -65,18 +98,7 @@ class _QuizPageState extends State<QuizPage> {
               padding: const EdgeInsets.symmetric(horizontal: 10.0),
               child: ElevatedButton(
                 onPressed: () {
-                  bool? currectAnswer = quizeBrain.currectAnswer();
-
-                  if (currectAnswer == true) {
-                    // ignore: avoid_print
-                    print("user currect");
-                  } else {
-                    // ignore: avoid_print
-                    print("user not currect");
-                  }
-                  setState(() {
-                    quizeBrain.nextQuestion();
-                  });
+                  checkAnswer(true);
                 },
                 child: const Text(
                   "True",
@@ -97,26 +119,16 @@ class _QuizPageState extends State<QuizPage> {
               padding: const EdgeInsets.symmetric(horizontal: 10.0),
               child: ElevatedButton(
                 onPressed: () {
-                  bool? currectAnswer = quizeBrain.currectAnswer();
-
-                  if (currectAnswer == false) {
-                    // ignore: avoid_print
-                    print("user currect");
-                  } else {
-                    // ignore: avoid_print
-                    print("user not currect");
-                  }
-                  setState(() {
-                    quizeBrain.nextQuestion();
-                  });
+                  checkAnswer(false);
                 },
                 child: const Text(
                   "False",
                   style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                 ),
                 style: ElevatedButton.styleFrom(
-                    primary: Colors.red,
-                    minimumSize: const Size(double.infinity, 40)),
+                  primary: Colors.red,
+                  minimumSize: const Size(double.infinity, 40),
+                ),
               ),
             ),
           ),
--- quizzler_flutter/lib/quiz_brain.dart
+++ quizzler_flutter/lib/quiz_brain.dart
@@ -4,13 +4,13 @@ class Quizbrain {
   int _quetionsNumber = 0;
   final List<Question> _questionsBank = [
     Question(
-      q: 'You can lead a cow down stairs but not up stairs.',
-      a: false,
+      questionText: 'You can lead a cow down stairs but not up stairs.',
+      questionAnswer: false,
     ),
     Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true)
+        questionText: 'Approximately one quarter of human bones are in the feet.',
+        questionAnswer: true),
+    Question(questionText: 'A slug\'s blood is green.', questionAnswer: true)
   ];
 
   void nextQuestion() {
@@ -25,5 +25,23 @@ class Quizbrain {
 
   bool? currectAnswer() {
     return _questionsBank[_quetionsNumber].questionAnswer;
+  }
+
+  
+
+  bool? isFinished() {
+    if (_quetionsNumber >= _questionsBank.length - 1) {
+      // ignore: avoid_print
+      print('Now returning true');
+      return true;
+
+    } else {
+      return false;
+    }
+  }
+
+  
+  void reset() {
+    _quetionsNumber = 0;
   }
 }
